2d8656cf96ce053cb08167f1515ca9c58cfb8c62
Abdur Rafay Baig <email2rafaybaig@gmail.com>
2025-11-03 23:36:24 +0500

    Test Suit 1

---
+++ FastTestsSuite.java
@@ -0,0 +1,10 @@
+package com.arb.statefull;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ TestDivideCalculator.class, TestAddCalculator.class })
+public class FastTestsSuite {
+
+}
