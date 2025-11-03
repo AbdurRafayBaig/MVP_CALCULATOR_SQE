ed626977222ea5a323c327c4c4998fde42c95424
Abdur Rafay Baig <email2rafaybaig@gmail.com>
2025-11-03 23:37:22 +0500

    Test Suit 3

---
+++ IntegeratedSuite.java
@@ -0,0 +1,11 @@
+package com.arb.statefull;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ CsvDataDrivenCalculatorTests.class, TestAddCalculator.class, TestDivideCalculator.class,
+		TestSubtractCalculator.class })
+public class IntegeratedSuite {
+
+}
